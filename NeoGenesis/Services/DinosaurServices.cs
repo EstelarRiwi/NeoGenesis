6d4c4332d4baa6ced874ccbe3de3a0118f47ab4e
UlithG18 <ulith.software@gmail.com>
2026-04-09 17:19:31 -0500

    feat: add Utils folder and initian services

---
+++ NeoGenesis/Services/DinosaurServices.cs
@@ -0,0 +1,35 @@
+using NeoGenesis.ModelsExample;
+using NeoGenesis.Utils;
+
+namespace NeoGenesis.Services
+{
+    public static class DinosaurServices
+    {
+        public static void DinosaurRegister()
+        {
+            Console.WriteLine("\t\t--- Dinosaur Register ---");
+
+            string id = Validations.ValidateId("\n\n\tGive me an Id: ");
+                            
+            if (DbContext.dinosaurs.Any(driver => driver.Id == id)) 
+            {
+                Console.WriteLine("A Dinosaur with this ID already exists"); 
+                return;
+            }
+                            
+            string name = Validations.ValidateName("\n\n\tGive me a Name: ");
+
+            int age = Validations.ValidateId("\n\n\tGive me an Age: ");
+            
+            Driver newDriver = new Driver (id, name, license);
+                            
+            DbContext.drivers.Add(newDriver);
+            DbContext.SaveChanges();
+
+            Console.WriteLine("\n\n\tThe Driver you added is: "); 
+            newDriver.PrintInfo(); 
+            
+        }
+    }
+}
+
---
+++ NeoGenesis/Utils/Validations.cs
@@ -0,0 +1,110 @@
+namespace NeoGenesis.Utils
+{
+        public static class Validations
+    { 
+        public static string ValidateName(string message)
+        {
+            while (true)
+            {
+                Console.Write(message);
+                string name = Console.ReadLine()?.Trim() ?? "";
+
+                if (string.IsNullOrEmpty(name))
+                {
+                    Console.WriteLine("Name cannot be empty");
+                    continue;
+                }
+
+                bool isValid = true;
+                foreach (char l in name)
+                {
+                    if (!char.IsLetter(l) && l != ' ' && l != '-')
+                    {
+                        isValid = false;
+                        break;
+                    }
+                }
+
+                if (!isValid)
+                {
+                    Console.WriteLine("Name can only contain letters, spaces, or hyphens");
+                    continue;
+                }
+
+                return name;
+            }
+        }
+
+        public static string ValidateEmpty(string message)
+        {
+            while (true)
+            {
+                Console.Write(message);
+                string value = Console.ReadLine()?.Trim() ?? "";
+        
+                if (string.IsNullOrEmpty(value))
+                {
+                    Console.WriteLine("Value cannot be empty");
+                    continue;
+                }
+                    
+                return value;
+            }
+        }
+        
+        public static int ValidateInteger(string message, int min = 1, int? max = null)
+            {
+                while (true)
+                {
+                    Console.Write(message);
+                    string input = Console.ReadLine()?.Trim() ?? "";
+        
+                    if (!int.TryParse(input, out int value))
+                    {
+                        Console.WriteLine("Invalid digit, please enter a valid number");
+                        continue;
+                    }
+
+                    if (value < min || value > max)
+                    {
+                        Console.WriteLine($"Please enter a value between {min} and {max}");
+                        continue;
+                    }
+        
+                    return value;
+                }
+            }
+        
+        public static string ValidateId(string message, int? min = null, int? max = null)
+        {
+            while (true)
+            {
+                Console.Write(message); 
+                string id = Console.ReadLine()?.Trim() ?? "";
+                        
+                if (string.IsNullOrEmpty(id))
+                {
+                    Console.WriteLine("ID cannot be empty");
+                    continue;
+                }
+        
+                if (!int.TryParse(id, out _))
+                { 
+                    Console.WriteLine("Invalid digits, please enter a valid ID"); 
+                    continue;
+                }
+                    
+                int idLength = id.Length;
+                    
+                if ((min != null && idLength < min) || (max != null && idLength > max)) 
+                { 
+                    Console.WriteLine($"Please enter an ID with {min} until {max} digits"); 
+                    continue;
+                }
+                    
+                return id;
+            }
+        }
+    }
+}
+
