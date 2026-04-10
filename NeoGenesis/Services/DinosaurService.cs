70ac5af7e344e86701f19f2c3d1bbf666b9d88cb
JuanFelipe0215 <juanfe13q@gmail.com>
2026-04-10 15:26:04 -0500

    feat: add features to the DinosaurService

--- NeoGenesis/Services/DinosaurService.cs
+++ NeoGenesis/Services/DinosaurService.cs
@@ -1,7 +1,27 @@
-﻿namespace NeoGenesis.Services;
+﻿using Microsoft.EntityFrameworkCore;
+using NeoGenesis.Models;
 using NeoGenesis.Data;
+
+namespace NeoGenesis.Services;
 
 public class DinosaurService
 {
+    private readonly DataContext _context;
+
+    public DinosaurService(DataContext context)
+    {
+        _context = context;
+    }
     
+    /*
+    public List<Dinosaur> DetailId(int id)
+    {
+        return _context.Dinosaurs
+            .Include(d => d.Name)
+            .Where(d => d.Id = id)
+            .ToList<Dinosaur>();
+
+    }
+    */
+
 }
