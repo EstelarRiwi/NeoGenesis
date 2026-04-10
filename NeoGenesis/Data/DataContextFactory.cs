b3766560ca27c861e1cac1e03d13febd46e3d52e
JuanFelipe0215 <juanfe13q@gmail.com>
2026-04-10 13:57:27 -0500

    feat: add files .Net

---
+++ NeoGenesis/Data/DataContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace NeoGenesis.Data;
+
+public class DataContextFactory : IDesignTimeDbContextFactory<DataContext>
+{
+    public DataContext CreateDbContext(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
+        optionsBuilder.UseMySql(
+            connectionString,
+            ServerVersion.AutoDetect(connectionString)
+        );
+
+        return new DataContext(optionsBuilder.Options);
+    }
+}
