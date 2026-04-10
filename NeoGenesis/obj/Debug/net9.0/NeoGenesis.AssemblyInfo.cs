b3923cf2ea6f55f5c47659a9740fed96006f0f2d
JuanFelipe0215 <juanfe13q@gmail.com>
2026-04-10 09:42:05 -0500

    feat: Complete configuration of the dbcontext

--- NeoGenesis/obj/Debug/net9.0/NeoGenesis.AssemblyInfo.cs
+++ NeoGenesis/obj/Debug/net9.0/NeoGenesis.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NeoGenesis")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22347f8ee822042b514f05e59cccbea764e79fe2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48cffe3613dc317e259965bfd592d865ee628cf4")]
 [assembly: System.Reflection.AssemblyProductAttribute("NeoGenesis")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NeoGenesis")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
