0ff388a5d3d057707591b79bed0a04c10308639e
JuanFelipe0215 <juanfe13q@gmail.com>
2026-04-11 17:39:46 -0500

    feat: consultas linq count

--- NeoGenesis/Services/DinosaurService.cs
+++ NeoGenesis/Services/DinosaurService.cs
@@ -21,6 +21,42 @@ public class DinosaurService
 
     }
     
+    public Dinosaur? DetailRegisterCode(string code)
+    {
+        return _context.Dinosaurs
+            .Include(d => d.Zone)
+            .FirstOrDefault(d => d.RegisterCode == code);
+    }
+
+    public List<Dinosaur> GetByZone(string zoneName)
+    {
+        return _context.Dinosaurs
+            .Include(d => d.Zone)
+            .Where(d => d.Zone.Name.ToLower() == zoneName.ToLower())
+            .ToList(); 
+    }
+
+    public List<Dinosaur> GetBySector(string sectorName)
+    {
+        return _context.Dinosaurs
+            .Include(d => d.Zone)
+            .ThenInclude(z => z.Sector)
+            .Where(d => d.Zone.Sector.Name.ToLower() == sectorName.ToLower())
+            .ToList();
+    }
+
+    public int CountDinosaurs()
+    {
+        return _context.Dinosaurs
+            .Count();
+    }
+
+    public int CountDinosaurByZone(string zoneName)
+    {
+        return _context.Dinosaurs
+            .Where(d => d.Zone.Name == zoneName)
+            .Count();
+    }
     
 
 }
